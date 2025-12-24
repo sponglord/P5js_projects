7c20175201a38485deebf75c67b45c9516d94599
sponglord <nickspong68@gmail.com>
2025-12-24 07:07:41 +0100

    Bigger grid. Use number of cells in motion to switch phases

--- GeorgNees/sketch_4_multiPhaseGrids.js
+++ GeorgNees/sketch_4_multiPhaseGrids.js
@@ -22,7 +22,7 @@ const _centerY = _canvasH / 2;
 
 let _maxDistFromCenter = null;
 
-const _maxGridSize = 15; // 15
+const _maxGridSize = 17; // 15
 
 const _gridSizes = [];
 const _gridStartPositions = [];
@@ -183,8 +183,10 @@ function draw() {
 	if (_checkForCollision) {
 		// Radial grid - set cells adrift
 		if (_phase >= 3) {
-			if (checkForCellMotion(_cells[0])) {
-				// console.log('### HAS MOVED ENOUGH AGAIN AGAIN!!!!!');
+			// If a quarter of the cells have moved twice a square size from target
+			const ncim = getNumCellsInMotion(2);
+			console.log('### getNumCellsInMotion:: =', ncim);
+			if (ncim > _numCells / 4) {
 				setPhase(4);
 				return;
 			}
@@ -192,8 +194,10 @@ function draw() {
 
 		// Rotated cells - ready to go to radial grid
 		if (_phase === 2) {
-			if (checkForCellMotion(_cells[0])) {
-				// console.log('### HAS MOVED ENOUGH AGAIN!!!!!');
+			// If a fifth of the cells have moved twice a square size from target
+			const ncim = getNumCellsInMotion(2);
+			console.log('### getNumCellsInMotion:: =', ncim);
+			if (ncim > _numCells / 5) {
 				setPhase(3); // send to radial grid
 				return;
 			}
@@ -201,9 +205,10 @@ function draw() {
 
 		// If in phase1 (regular grid) - ready to rotate cells
 		if (_phase === 1) {
-			// check if first cell has moved enough
-			if (checkForCellMotion(_cells[0])) {
-				// console.log('### HAS MOVED ENOUGH!!!!!');
+			// If a quarter of the cells have moved a square size from target
+			const ncim = getNumCellsInMotion();
+			console.log('### getNumCellsInMotion:: =', ncim);
+			if (ncim > _numCells / 4) {
 				setPhase(2);
 				return;
 			}
@@ -686,9 +691,23 @@ function resetCellInCollisionProp() {
 	}
 }
 
-// monitor if "test" cell has moved a sufficient distance (a (half) square on the x & y from where it was)
-function checkForCellMotion(pCell, pDivider = 1) {
-	let moveDist = _sqSize / pDivider; // / 2;
+function getNumCellsInMotion(pCellProportion = 1) {
+	let count = 0;
+	for (let i = 0; i < _cells.length; i++) {
+		const cell = _cells[i];
+		if (checkForCellMotion(cell, pCellProportion)) {
+			count++;
+		}
+	}
+	return count;
+}
+
+/**
+ * Check if a cell has moved a given proportion of its size on the x & y axes
+ * away from its target (resting) position
+ */
+function checkForCellMotion(pCell, pCellProportion = 1) {
+	let moveDist = _sqSize * pCellProportion;
 	let { x, y, targetX, targetY } = pCell;
 
 	const xMin = targetX - moveDist;
